9597cf8686b469e8cbccf19cc549807a0ad268f6
Nathan Pham <nathanpham.me@gmail.com>
2021-05-17 11:57:10 -0700

    snake

--- public/js/game/Snake.js
+++ public/js/game/Snake.js
@@ -1,10 +1,20 @@
 import Object from "./base/Object.js"
+import config from "../config.js"
 
 export default class Snake extends Object {
     name = "snake"
     body = []
 
     render(ctx, scale) {
-        
+        ctx.fillStyle = config.color.snake
+
+        for(const cell of this.body) {
+            const { x, y } = cell
+            ctx.fillStyle(x * scale, y * scale, scale, scale)
+        }
+    }
+
+    add(x, y) {
+        this.body.push({ x, y })
     }
 }
